00578b506e1e7bdbf5bbaccc83df658941d47f6a
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2021-06-13 03:17:17 +0700

    add update info user

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -45,6 +45,30 @@ module.exports.login = async (req, res, next) => {
     }
 }
 
+module.exports.changePassword = async (req, res, next) => {
+    const {customerId} = req.params
+    try {
+        const {oldPassword, newPassword} = req.body;
+        const customer = await Customer.findById(customerId);
+        if (customer) {
+            console.log(oldPassword)
+            console.log(customer.password)
+            const validPassword = await bcrypt.compare(oldPassword, customer.password);
+            if(validPassword) {
+                const hashPassword = await bcrypt.hash(newPassword, saltRounds);
+                customer.password = hashPassword
+                await customer.save();
+                return res.status(200).json({success: true, msg: 'Đổi mật khẩu thành công'})
+            }
+            return res.status(200).json({success: false, msg: 'Mật khẩu cũ không đúng'})
+        }
+        return res.status(200).json({success: false, msg: 'Không tìm thấy tài khoản'})
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({success: false, msg: 'Đã có lỗi xảy ra'})
+    }   
+}
+
 module.exports.getInfo = async (req, res, next) => {
     const {customerId} = req.params
     try {
@@ -67,4 +91,39 @@ module.exports.getInfo = async (req, res, next) => {
         console.log(error);
         return res.status(400).json({success: false, msg: 'Đã có lỗi xảy ra'})
     }
+}
+
+
+
+module.exports.updateInfo = async (req, res, next) => {
+    const {customerId} = req.params
+    const user = req.body
+    try {
+        const p1 = new Promise((res,rej) => 
+                Customer.findById(customerId).select('-password').then((value) => res(value))
+            )
+        const p2 = new Promise((res,rej) => 
+                Customer.findOne({"phone": user.phone}).then((value) => res(value))
+            )
+        var [findId, findPhone] = await Promise.all([p1,p2])
+        if(findId){
+            if ((findPhone && findPhone._id == customerId) || !findPhone) {
+                findId.avatar = user.avatar;
+                findId.username = user.username;
+                findId.phone = user.phone;
+                findId.addres = user.addres;
+                await findId.save()
+                res.status(200).json({success: true, msg: 'Thay đổi thông tin thành công', doc: findId})
+            }
+            else{
+                console.log(findPhone)
+                console.log(customerId)
+                res.status(200).json({success: false, msg: 'Số điện thoại này đã tồn tại'})
+            } 
+        }
+        else return res.status(200).json({success: false, msg: 'Không tìm thấy tài khoản'})
+    } catch(e) {
+        console.log(e);
+        return res.status(400).json({success: false, msg: 'Đã có lỗi xảy ra'})
+    }   
 }
--- routers/CustomerRouter.js
+++ routers/CustomerRouter.js
@@ -5,6 +5,10 @@ route.post('/create', customerController.create)
 
 route.post('/login', customerController.login)
 
+route.post('/:customerId/update/password', customerController.changePassword)
+
 route.get('/:customerId/getInfo', customerController.getInfo)
+
+route.post('/:customerId/update/info', customerController.updateInfo)
 
 module.exports = route;
